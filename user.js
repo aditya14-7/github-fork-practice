6d043dc32d919bc8e0647c761ce46cbe8bf1571f
Harshvardhan Maurya <112819985+Harshvardhan123maurya@users.noreply.github.com>
2022-09-20 10:24:40 -0700

    Add files via upload

--- user.js
+++ user.js
@@ -1,19 +1,19 @@
 import React from "react";
 import UserItem from "./UserItem";
+import Loader from "../layouts/Loader";
 const Users = (props) => {
+    if(props.loading){
+        return (
+    
+
+            <Loader />
+        )
+    }
     return (
         <div className="container" style={userStyle}>
-        
+        <div className="" style={userStyle}/>
             {props.userData.map(user => (
                 <UserItem userData={user} />
+                <UserItem userData={user}key={user.id}/>
             ))}
-        </div>
-    )
-}
-const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem',
-}
-
-export default Users;
+            </div>
